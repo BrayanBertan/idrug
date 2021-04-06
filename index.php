b4c5bb814415f50601047f45d01bed18276f7a73
brayanbertan <brayanbertan@gmail.com>
2021-04-06 15:47:53 -0300

    layout home

--- index.php
+++ index.php
@@ -1,36 +1,53 @@
 <!DOCTYPE html>
 <html lang="pt-BR">
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="estilo.css">
-    <title>Document</title>
-</head>
-<div class="inicio">
-
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="estilo.css">
+        <title>Document</title>
+    </head>
     <body>
-
-        <div class="cervejas"></div>
-        <nav>
-            <ul>
-                <li><a href="cervejas.html">Cervejas</a></li>
-                <li><a href="">Sobre</a></li>
-                <li><a href="contato.html">Contato</a>
-            </ul>
-        </nav>
-        <div class="cervejas"></div>
-
-        <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
-        <div class="home"> CONHEÇA UM NOVO CONCEITO EM CERVEJA ARTESANAL!</div>
-        <br><br><br><br><br><br><br><br><br>
-        <p class="redes">Acompanhe-nos nas redes sociais!</p>
-        <img class="redesSociais" src="instagram.png" alt="">
-        <img class="redesSociais" src="facebook.png" alt="">
-</div>
-<footer>
-    CERVEJARIA
-</footer>
-</body>
+        <div class="header">
+            <img  src="assets/drugstore.png" alt="">
+            <div class="div-usuario">
+                <button type="button"><img  src="assets/user.png" alt="">Minha Conta</button>
+                <button type="button"><img  src="assets/carts.png" alt="">Meu Carrinho</button>
+            </div>
+            <div class="div-filtros">
+                <form action="/action_page.php">
+                    <input type="text" name="pesquisa" placeholder="     Pesquisa">
+                    <select name="" id="">
+                        <option value="">Teste</option>
+                    </select>
+                    <input class="faixa-preco" type="number" name="min" placeholder=" preço minimo">
+                    <input class="faixa-preco" type="number" name="max" placeholder=" preço máximo">
+                    <button type="submit"><img  src="assets/search.png" alt=""></button>
+                </form>
+            </div>
+            
+        </div>
+        <div class="conteudo">
+            <?php
+            for ($i=0; $i < 25; $i++) { 
+        
+           
+            ?>
+            <div class="produto">
+                <img src="assets/drugstore.png" alt="Avatar" style="width:100%">
+                <div class="info">
+                    <h4><b>Carga Gillette Mach 3 c/ 4 Unidades</b></h4>
+                    <p>R$39,10</p>
+                    <button>Comprar</button>
+                </div>
+            </div>
+            <?php
+            }
+            ?>
+        </div>
+        <footer>
+            IDRUG
+        </footer>
+        </body>
 
 </html>
