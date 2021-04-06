0569d42e5f8bb7e935e3e9559805cc0d94b27cfa
brayanbertan <brayanbertan@gmail.com>
2021-04-06 11:21:40 -0300

    first commit

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="estilo.css">
+    <title>Document</title>
+</head>
+<div class="inicio">
+
+    <body>
+
+        <div class="cervejas"></div>
+        <nav>
+            <ul>
+                <li><a href="cervejas.html">Cervejas</a></li>
+                <li><a href="">Sobre</a></li>
+                <li><a href="contato.html">Contato</a>
+            </ul>
+        </nav>
+        <div class="cervejas"></div>
+
+        <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+        <div class="home"> CONHEÇA UM NOVO CONCEITO EM CERVEJA ARTESANAL!</div>
+        <br><br><br><br><br><br><br><br><br>
+        <p class="redes">Acompanhe-nos nas redes sociais!</p>
+        <img class="redesSociais" src="instagram.png" alt="">
+        <img class="redesSociais" src="facebook.png" alt="">
+</div>
+<footer>
+    CERVEJARIA
+</footer>
+</body>
+
+</html>
