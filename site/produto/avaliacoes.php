9cceaf91897a45dd2c380194f447e5b3e9993878
brayanbertan <brayanbertan@gmail.com>
2021-04-11 14:15:32 -0300

    layout do formulario de novo comentario

---
+++ site/produto/avaliacoes.php
@@ -0,0 +1,32 @@
+            <h2><b>Avaliações</b></h2><br><br>
+            <br>
+            <div class="avaliacoes">
+                <form action="">
+                    <label for="comentario">Comentario</label>
+                    <input type="text" name="comentario" id="comentario" maxlength="255">
+                    <label for="nota">Nota</label>
+                    <input type="number" name="nota" id="nota" >
+                    <button type="submit" disabled>enviar</button>
+                </form>
+                <?php 
+                    if($quantidade_avaliacoes == 0){
+                        echo 'Nenhuma avaliação disponivel para esse produto';
+                    }else
+                ?>
+                <?php
+                foreach ($avaliacoes as $item) { 
+                ?>
+                <div class="avaliacao">
+                    <div class="dados"> 
+                        <img src="<?php echo $item['foto']?>" >
+                        <h3><?php echo $item['nome']?></h3>
+                        <h2><?php echo $item['nota']?></h2>
+                    </div>
+                   <div class="comentario">
+                   <p><?php echo $item['comentario']?></p>
+                   </div>
+                </div>
+                <?php
+                    }
+                ?>
+            </div>
--- site/produto/produto.php
+++ site/produto/produto.php
@@ -79,30 +79,7 @@
                         </div>
                 </div>
             </div>
-            <h2><b>Avaliações</b></h2><br><br>
-            <br>
-            <div class="avaliacoes">
-                <?php
-                foreach ($avaliacoes as $item) { 
-            
-                ?>
-                <div class="avaliacao">
-                    <div class="dados"> 
-                        <img src="<?php echo $item['foto']?>" >
-                        <h3><?php echo $item['nome']?></h3>
-                        <h2><?php echo $item['nota']?></h2>
-                    </div>
-                   <div class="comentario">
-                   <p><?php echo $item['comentario']?></p>
-                   </div>
-                </div>
-                <?php
-                    }
-
-                    if($quantidade_avaliacoes == 0)
-                        echo "Nenhuma avaliação disponivel para esse produto";
-                ?>
-            </div>
+           <?php include('avaliacoes.php')?>
         </div>
         <footer>
             IDRUG
